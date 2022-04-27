f9d8bd92635ba92143aadd1e69a1205bb303450d
vbaswin <aswinvb10a@gmail.com>
2022-04-27 20:04:02 +0530

    Fedora again dude

--- c++/mst_cri_skls/16_templates/1_function_templates/2.cpp
+++ c++/mst_cri_skls/16_templates/1_function_templates/2.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 using namespace std;
 
+template<class Type1, class Type2> 
+Type1 sum(Type1 a, Type2 b){
+    Type1 r = a + b;
+    return r;
+}
+_
 int main() {
     cout << sum(1, 10) << "\n";
     cout << sum(1,10.5) << "\n";
@@ -10,6 +16,11 @@ int main() {
     cout << sum('A', 1) << "\n";
     cout << sum(1, 'A') << "\n";
 
-    // cout << sum("I am", "Mostafa") << "\n";
-    cout << sum(string("I am"), string("Mostafa")) << "\n";
+    // cout << sum("I am", "Mostafa") << "\n"; //  CE: char*
+    cout << sum(string("I am"), string(" Mostafa")) << "\n";
 }
+
+/*
+   compiler generates different functions for different data types
+   here 3 types int (int int) , double, char, string
+*/
